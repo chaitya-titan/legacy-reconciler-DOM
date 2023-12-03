29fb9f0fa5dea44af98b6b1ddd39beadee8c511d
Chaitya W <chaityaw@Chaityas-MacBook-Air.local>
2023-12-03 15:35:49 +0530

    updating dyncamically

--- index.js
+++ index.js
@@ -13,29 +13,32 @@ let todos = [
   },
 ];
 
-var parent = document.getElementById("area");
-parent.innerHTML = "";
-let child;
+function updateTodosOnScreen() {
+  var parent = document.getElementById("area");
+  parent.innerHTML = "";
+  let child;
 
-todos.forEach((todo) => {
-  child = document.createElement("div");
-  var title = document.createElement("h1");
-  var desc = document.createElement("p");
-  var btn = document.createElement("button");
-  title.innerHTML = todo.title;
-  desc.innerHTML = todo.desc;
-  btn.innerHTML = "Delete";
-  child.appendChild(title);
-  child.appendChild(desc);
-  child.appendChild(btn);
-  parent.appendChild(child);
-});
+  todos.forEach((todo) => {
+    child = document.createElement("div");
+    var title = document.createElement("h1");
+    var desc = document.createElement("p");
+    var btn = document.createElement("button");
+    title.innerHTML = todo.title;
+    desc.innerHTML = todo.desc;
+    btn.innerHTML = "Delete";
+    child.appendChild(title);
+    child.appendChild(desc);
+    child.appendChild(btn);
+    parent.appendChild(child);
+  });
+}
 
-console.log(parent.innerHTML);
+updateTodosOnScreen();
 
 function onPress() {
   var title = document.getElementById("title").value;
   var desc = document.getElementById("desc").value;
   todos.push({ title: title, desc: desc });
+  updateTodosOnScreen();
   console.log(todos);
 }
